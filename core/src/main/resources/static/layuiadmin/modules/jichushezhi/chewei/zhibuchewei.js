e69dbbd035e44bad8b774bc1ae594c78efebd369
luruijing <734991343@qq.com>
2019-04-18 08:53:39 +0800

    车位表结构修改

--- core/src/main/resources/static/layuiadmin/modules/jichushezhi/chewei/zhibuchewei.js
+++ core/src/main/resources/static/layuiadmin/modules/jichushezhi/chewei/zhibuchewei.js
@@ -4,6 +4,27 @@ layui.define(['table', 'form', 'laydate'], function(exports){
         ,form = layui.form
         ,laydate = layui.laydate;
 
+    //筛选条件初始化
+
+
+
+    var cols =  [
+        {fixed:'left',checkbox:true}
+        ,{field: 'id', title: 'id',hide:true}
+        ,{field: 'rownum', title: '',width:50}
+        ,{field: 'name',sort:true, title: '参数类别'}
+        ,{field: 'csm',sort:true, title: '参数名'}
+        ,{field: 'danwei',sort:true, title: '参数单位'}
+        ,{field: 'cunchuzhouqi',sort:true, title: '存储周期(秒)'}
+        ,{field: 'cunchushichang',sort:true, title: '存储时长(时)'}
+        ,{field: 'baojing_flag',sort:true, title: '是否报警'}
+        ,{field: 'zhanshi_flag',sort:true, title: '是否报警'}
+        ,{field: 'cunchu_flag',sort:true, title: '是否报警'}
+        ,{field: 'xuhao',sort:true, title: '排序号'}
+    ];
+
+    //根据form获取参数
+    initTable_all("table", 'jichushuju/shebei/shebeiparam/findAll', 'get',[cols], table, "form");
 
 
 
