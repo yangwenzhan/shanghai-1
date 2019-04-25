a8a33a21a163668292fa0ef8778dab8b60004d8b
xingxiaoshuai <xingxiaoshuai.vip@icloud.com>
2019-04-25 10:42:40 +0800

    1。津田驹实时数据； 2。历史班次，当前班次表

---
+++ core/src/main/java/com/tianqiauto/textile/weaving/model/base/PB_History.java
@@ -0,0 +1,42 @@
+package com.tianqiauto.textile.weaving.model.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+/**
+ * @Author xingxiaoshuai
+ * @Date 2019-02-14 16:22
+ * @Version 1.0
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "base_pb_history")
+@EqualsAndHashCode(exclude = {"gongxu"})
+@ToString(exclude = {"gongxu"})
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class PB_History {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @ManyToOne
+    @JoinColumn(name = "gongxu_id")
+    private Gongxu gongxu;
+
+    private Date  riqi;
+    private Dict banci;
+    private Dict lunban;
+
+    private Date bancikaishishijian;
+    private Date bancijieshushijian;
+
+
+}
