9202d64dd0c0f7b44ce61cf6b3c5e1b200d8fb73
luruijing <734991343@qq.com>
2019-04-29 20:22:48 +0800

    布辊追溯

--- core/src/main/java/com/tianqiauto/textile/weaving/service/bugun/BuGunJiLuService.java
+++ core/src/main/java/com/tianqiauto/textile/weaving/service/bugun/BuGunJiLuService.java
@@ -3,6 +3,10 @@ package com.tianqiauto.textile.weaving.service.bugun;
 import com.tianqiauto.textile.weaving.model.sys.BuGun;
 import com.tianqiauto.textile.weaving.repository.BuGunRepository;
 import com.tianqiauto.textile.weaving.util.model.ModelUtil;
+import com.tianqiauto.textile.weaving.util.procedure.core.ProcedureParamUtlis;
+import com.tianqiauto.textile.weaving.util.procedure.model.ProcedureContext;
+import com.tianqiauto.textile.weaving.util.procedure.service.BaseService;
+import com.tianqiauto.textile.weaving.util.result.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +31,9 @@ public class BuGunJiLuService {
     @Autowired
     private BuGunRepository buGunRepository;
 
+    @Autowired
+    private BaseService baseService;
+
     public Specification getSpecification(BuGun buGun){
         ModelUtil mu = new ModelUtil(buGun);
         return (Specification<BuGun>) (root, criteriaQuery, criteriaBuilder) -> {
@@ -49,6 +56,14 @@ public class BuGunJiLuService {
     public Page<BuGun> findAll(BuGun buGun, Pageable pageable){
         Page<BuGun> buGuns = buGunRepository.findAll(getSpecification(buGun),pageable);
         return buGuns;
+    }
+
+    //布辊追溯  type:1左轴，2右轴
+    public Result buGunZhuiSu(String shift_zhou_id,String type){
+        ProcedureParamUtlis ppu=new ProcedureParamUtlis();
+        ppu.addInVarchar(shift_zhou_id).addInVarchar(type);
+        ProcedureContext pro=baseService.callProcedure("pc_bugun_zhuisu", ppu.getList());
+        return Result.ok(pro.getDatas());
     }
 
 
